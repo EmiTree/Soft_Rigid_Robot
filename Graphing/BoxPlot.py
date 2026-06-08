4ac2c5e676066b74d99f8d98da0ed050fb0e63b5
LeonStil <L.P.Stil@student.tudelft.nl>
2026-06-08 10:54:26 +0200

    new file:   Graphing/BoxPlot.py

---
+++ Graphing/BoxPlot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import pandas as pd
+
+df = pd.read_csv("Testvaluestense2.csv")
+print(df)
+
+
+
+
+#with open("Testvalues.csv", "r", newline="") as f:
+#    data = csv.reader(f)
+#    #print(data)
+#    for row in data:
+#        print(row)
