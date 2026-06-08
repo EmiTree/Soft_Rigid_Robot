2b4315b578c9fe13ab766345ab3535d74001101a
S-Merle <m.l.stokhof@student.tudelft.nl>
2026-06-08 11:49:12 +0200

    modified:   Troubleshooting/IMU/Testvalues.py

--- Troubleshooting/IMU/Testvalues.py
+++ Troubleshooting/IMU/Testvalues.py
@@ -3,7 +3,7 @@ import serial
 import time
 
 ser = serial.Serial('COM3', 115200)  # Adjust baud rate if needed
-
+setpoint = -5.5 #change set point after every balancing change
 start_time = time.time()
 
 with open("Testvaluestense2.csv", "w", newline="") as csvfile:
@@ -13,7 +13,7 @@ with open("Testvaluestense2.csv", "w", newline="") as csvfile:
         if ser.in_waiting > 0:
             data = ser.readline().decode('utf-8').strip()
             if data.startswith("A"):
-                angle = float(data[1:])  # Remove the "A%" prefix and convert to float   
+                angle = float(data[1:]) + setpoint  # Remove the "A%" prefix and convert to float   
                 writer.writerow([
                     time.time() - start_time,
                     angle
